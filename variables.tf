ad50b62a8b307cd77ccdc7905f1a302219d3b68a
Kavya-2697 <kavya.02697@gmail.com>
2025-06-26 13:41:35 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "security_group" { 
     description = "Name of security group" 
-    default     = "jenkins-sgroup-dec-2024" 
+    default     = "sgr-037d74d46009f0652" 
 }
 
 variable "tag_name" { 
