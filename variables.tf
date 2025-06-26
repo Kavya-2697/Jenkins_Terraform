1450f311a41610f07937cad49390eff59125e3bb
Kavya-2697 <kavya.02697@gmail.com>
2025-06-26 13:37:17 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "aws_region" {
        description = "The AWS region to create things in." 
-       default     = "us-east-2" 
+       default     = "us-east-1" 
 }
 
 variable "key_name" { 
@@ -24,7 +24,7 @@ variable "tag_name" {
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-0ddda618e961f2270" 
+    default     = "ami-05ffe3c48a9991133" 
 }
 variable "versioning" {
     type        = bool
