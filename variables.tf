85c63b53e0624fc525817c7834bae0c930d7ba78
Kavya-2697 <kavya.02697@gmail.com>
2025-06-26 13:57:12 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "security_group" { 
     description = "Name of security group" 
-    default     = "sgr-037d74d46009f0652" 
+    default     = "sg-0cce0c68906c90505" 
 }
 
 variable "tag_name" { 
